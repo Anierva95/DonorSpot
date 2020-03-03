2f474240101b34d3d4a390feffe5efda2907f945
Max Kennedy <maxken512@gmail.com>
2020-03-03 14:14:20 -0500

    server/ db/ empty files

---
+++ schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS charity_app_db;
+
+CREATE DATABASE charity_app_db;
+
+USE charity_app_db;
+
+CREATE TABLE users(
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(25) NOT NULL,
+    passwd varchar(15) NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(40) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE charity_posts(
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(100) NOT NULL,
+    descript varchar(500) NOT NULL,
+    goal DECIMAL NOT NULL,
+    owner_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE transactions(
+    id int NOT NULL AUTO_INCREMENT,
+    amount decimal NOT NULL,
+    donor_id int NOT NULL,
+    charity varchar(100) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (donor_id) REFERENCES users(id),
+    FOREIGN KEY (charity) REFERENCES charity_posts(title)
+);
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require()(app);
+require()(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });

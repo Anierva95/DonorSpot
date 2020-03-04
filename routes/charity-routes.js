141d2a2632d3dddd725469dbe438be7695be5209
Henry Ni <henryni914@gmail.com>
2020-03-04 18:36:01 -0500

    created a route to render charity handlebar

--- routes/charity-routes.js
+++ routes/charity-routes.js
@@ -1,11 +1,28 @@
 const db = require("../models");
- 
-module.exports = function(app) {
-    app.get("/api/charity", function(req, res) {
+
+module.exports = function (app) {
+    app.get("/api/charity", function (req, res) {
         db.Charity.findAll({
-             include: [db.Users]
-        }).then(function(dbCharity) {
+            include: [db.Users]
+        }).then(function (dbCharity) {
             res.json(dbCharity);
+            // res.render("charity", dbCharity)
+        })
+    });
+    app.get("/", function (req, res) {
+        db.Charity.findAll({
+        }).then(function (dbCharity) {
+            // console.log(dbCharity);
+            let newCharity = []
+            dbCharity.forEach(element => {
+                let newData = element.dataValues
+                newCharity.push(newData)
+            });
+            var newObject = {
+                charities: newCharity
+            };
+            // console.log(newCharity);
+            res.render("charity", newObject)
         })
     })
-}
+};
--- schema.sql
+++ schema.sql
@@ -4,28 +4,28 @@ CREATE DATABASE charity_app_db;
 
 USE charity_app_db;
 
-CREATE TABLE users(
-    id int NOT NULL AUTO_INCREMENT,
-    username varchar(25) NOT NULL,
-    passwd varchar(15) NOT NULL,
-    first_name varchar(30) NOT NULL,
-    last_name varchar(40) NOT NULL,
-    PRIMARY KEY (id)
-);
-CREATE TABLE charity_posts(
-    id int NOT NULL AUTO_INCREMENT,
-    title varchar(100) NOT NULL,
-    descript varchar(500) NOT NULL,
-    goal DECIMAL NOT NULL,
-    owner_id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (owner_id) REFERENCES users(id)
-);
+-- CREATE TABLE users(
+--     id int NOT NULL AUTO_INCREMENT,
+--     username varchar(25) NOT NULL,
+--     passwd varchar(15) NOT NULL,
+--     first_name varchar(30) NOT NULL,
+--     last_name varchar(40) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+-- CREATE TABLE charity_posts(
+--     id int NOT NULL AUTO_INCREMENT,
+--     title varchar(100) NOT NULL,
+--     descript varchar(500) NOT NULL,
+--     goal DECIMAL NOT NULL,
+--     owner_id int NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (owner_id) REFERENCES users(id)
+-- );
 
-CREATE TABLE transactions(
-    id int NOT NULL AUTO_INCREMENT,
-    amount decimal NOT NULL,
-    charity_id int NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (donor_id) REFERENCES users(id),
-    FOREIGN KEY (charity_id) REFERENCES charity_posts(id)
+-- CREATE TABLE transactions(
+--     id int NOT NULL AUTO_INCREMENT,
+--     amount decimal NOT NULL,
+--     charity_id int NOT NULL,
+--     PRIMARY KEY (id),
+--     FOREIGN KEY (donor_id) REFERENCES users(id),
+--     FOREIGN KEY (charity_id) REFERENCES charity_posts(id)

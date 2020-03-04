47c56c4e6d40ef0c9827277e9a116d0f6c21c374
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-04 10:20:38 -0500

    Fix file structure

---
+++ models/charity.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) {
+    var Charity = sequelize.define("Charity", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1] //at least 1 letter for the charity name
+            }
+        },
+        descript: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1,500] //at least 1 length, but up to 500
+            }
+        },
+        goal: {
+            type: DataTypes.DECIMAL(10, 2), //length 10 and 2 decimal places
+            allowNull: false,
+            validate: {
+                min: 10.00,
+                max: 1000000.00
+            }
+        }
+    })
+};
---
+++ models/users.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+    var Users = sequelize.define("Users", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, //username must be unique
+            validate: {
+                len: [1] //at least 1 letter for the charity name
+            }
+        },
+        passwd: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1,500] //at least 1 length, but up to 500
+            }
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1,500] //at least 1 length, but up to 500
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1,500] //at least 1 length, but up to 500
+            }
+        },
+    })
+};

1cdbb2a90af3da496e972c3356f4e67652b5bdc2
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-05 16:08:26 -0500

    session storage for login works

---
+++ public/js/login-form.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var loginForm = $("#loginForm");
+    var username = $("#username");
+    var password = $("#password");
+    $(loginForm).on("submit", handleFormSubmit)
+    function handleFormSubmit() {
+        event.preventDefault();
+        var loginObj = {
+            username: username.val().trim(),
+            password: password.val().trim()
+        }
+        $.post("/api/users/login", loginObj).then(function (result) {
+            console.log(result);
+            if (result.id >= 1) {
+                sessionStorage.setItem("user", [JSON.stringify(result.id), JSON.stringify(result.username)]);
+                window.location.replace('/');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+        })
+    };
+});
--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,42 +1,17 @@
-// var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var path = require('path');
 const db = require("../models");
-module.exports = function (app){
-    app.use(session({
-        secret: 'secret',
-        resave: true,
-        saveUninitialized: true
-    }));
-    app.use(bodyParser.urlencoded({extended : true}));
-    app.use(bodyParser.json());
-    app.get('/login', function(request, response) {
-        response.sendFile(path.join(__dirname + '/login.html'));
-    });
-    app.post('/auth', function(request, response) {
-        var username = request.body.username;
-        var password = request.body.password;
+module.exports = function (app) {
+ app.post('/api/users/login', function (req, res) {
+var username = req.body.username;
+        var password = req.body.password;
         if (username && password) {
             db.Users.findOne({
-                where:{
-                    username:username,
-                    passwd:password
+                where: {
+                    username: username,
+                    passwd: password
                 }
-            }).then(function(results) {
-                if (results.dataValues.id>=1) {
-                    request.session.loggedin = true;
-                    request.session.username = username;
-                    response.redirect('/');
-                } else {
-                    response.send('Incorrect Username and/or Password!');
-                }			
-                response.end();
-                console.log(results.dataValues)
+            }).then(function (results) {
+res.json(results);
             });
-        } else {
-            response.send('Please enter Username and Password!');
-            response.end();
         }
     });
 }

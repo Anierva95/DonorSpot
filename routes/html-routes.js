ab8337ad17f60873bc6bf71bddfdbb4ebbb35b64
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-05 11:28:43 -0500

    added Charity routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,5 +3,9 @@ var path = require("path");
 module.exports = function(app) {
     app.get("/charityForm", function (req, res) {
         res.sendFile(path.join(__dirname, "../views/charityForm.html"));
-    })
+    });
+    app.get("/signup", function(req, res) {
+        res.sendFile(path.join(__dirname, "../views/signup.html"));
+    });
 }
+
